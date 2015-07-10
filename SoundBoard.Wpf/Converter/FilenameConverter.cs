4fbc844c68df4395a311aeee40869907ab899e68
Thomas Luijken <thomas.luyken@ict.nl>
2015-07-10 08:43:49 +0200

    Layout fixes for filenames

---
+++ SoundBoard.Wpf/Converter/FilenameConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace SoundBoard.Wpf.Converter
+{
+    #region Namespaces
+
+    using System;
+    using System.Linq;
+    using System.Windows.Data;
+
+    #endregion
+
+    public class FilenameConverter : IValueConverter
+    {
+        #region Private fields
+
+        private readonly string[] _extentions = {".mp3", ".wav"};
+
+        #endregion
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return _extentions.Aggregate(value.ToString(), (current, extention) => current.Replace(extention, ""));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        #endregion
+    }
+}
